c171e1ad851fa3d2a3c825cc3abf9ebf9b8659f0
Abdelrahman <bedoalii255@gmail.com>
2026-01-27 21:58:03 +0200

    create realestate model

---
+++ lib/features/home/data/models/realestate_model.dart
@@ -0,0 +1,44 @@
+class RealestateModel {
+  final String title;
+  final num price;
+  final RealestateType type;
+  final List<String> images;
+  final int roomsNumber;
+  final int bathroomsNumber;
+  final num area;
+  final String desc;
+  final String address;
+  final int id;
+  final DateTime date;
+
+  RealestateModel({
+    required this.title,
+    required this.price,
+    required this.type,
+    required this.images,
+    required this.roomsNumber,
+    required this.bathroomsNumber,
+    required this.area,
+    required this.desc,
+    required this.address,
+    required this.id,
+    required this.date,
+  });
+  factory RealestateModel.fromJson(Map<String, dynamic> json) {
+    return RealestateModel(
+      title: json['title'],
+      price: json['price'],
+      type: json['type'],
+      images: json['images'],
+      roomsNumber: json['roomsNumber'],
+      bathroomsNumber: json['bathroomsNumber'],
+      area: json['area'],
+      desc: json['desc'],
+      address: json['address'],
+      id: json['id'],
+      date: DateTime.parse(json['date']),
+    );
+  }
+}
+
+enum RealestateType { tamleek, egar }
