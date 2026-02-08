6e60a723f6f912f816659bb3216f78819c8110fc
Moaz911gt <waleedmoaz65@gmail.com>
2026-02-08 12:14:31 +0200

    any

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'package:reelstate/features/auth/presentation/view/sign_in_view.dart';
-import 'package:reelstate/firebase_options.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await Supabase.initialize(
-    url: 'https://tlymmshywyrmaawdgpct.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRseW1tc2h5d3lybWFhd2RncGN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4NzM5ODcsImV4cCI6MjA4NTQ0OTk4N30.HASvC7DaqNSmjUJHM1o3n-hyY4kcqyrRv6Lgkm8ncXY',
-  );
-  runApp(const RealState());
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:get_it/get_it.dart';
 import 'package:reelstate/core/services/fire_store_service.dart';
 import 'package:reelstate/core/services/get_it_service.dart';
 import 'package:reelstate/core/utils/models/realstate_address_model.dart';
@@ -115,14 +99,12 @@ void main() async {
   runApp(const MyApp());
 }
 
-class RealState extends StatelessWidget {
-  const RealState({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: SignInView()),
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       locale: const Locale('ar'),
       supportedLocales: const [Locale('ar'), Locale('en')],
